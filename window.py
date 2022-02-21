d7e2cdcacef126fbc76bd7dfabc13fb68f0c7cf7
Leena <leenajagdish19@gmail.com>
2022-02-21 12:18:44 +0530

    window

---
+++ window.py
@@ -0,0 +1,8 @@
+from tkinter import *
+imaginary_tech_root=Tk()
+imaginary_tech_root.geometry("644x434")
+imaginary_tech_root.minsize(200,100)
+imaginary_tech_root.maxsize(1000,900)
+python=Label(text="This is window and GUI")
+python.pack()
+imaginary_tech_root.mainloop()
